91476bb5465623b8c6e89cdfaac4c2be13d14869
JohnPaulZigterman <johnpaulzigterman@gmail.com>
2023-11-05 09:46:58 -0500

    updated models and foreign key relationships

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,22 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const { databaseVersion } = require('../../../01-Class-Content/13-ORM/01-Activities/23-Ins_One-to-Many/config/connection.js');
 
 class Tag extends Model {}
 
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
